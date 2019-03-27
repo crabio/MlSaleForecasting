d83bc4a1e19965236b393639da731760b27b4898
Ivan <shchukin.ivan.v@gmail.com>
2019-03-27 07:31:08 +0300

    Rebuild model

--- SalesForecasting.py
+++ SalesForecasting.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
-
-
+#%%
 import sys
 get_ipython().system('{sys.executable} -m pip install matplotlib pandas xlrd seaborn tqdm scikit-learn tensorflow keras protobuf')
 
 
-# In[2]:
-
-
+#%%
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -22,15 +16,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dense, BatchNormalization, LeakyReLU, Activation
 from tensorflow.python.keras.wrappers.scikit_learn import KerasRegressor
 
-
+#%% [markdown]
 # Чтение данных из Excel
 
-# In[3]:
-
-
+#%%
 sales = pd.read_excel('ДанныеДекабря_2017.xlsx', 'Продажи')
 sales = sales.set_index('Material')
 
@@ -47,18 +39,10 @@ hierarchy = pd.read_excel('hierarchy.xlsx')
 
 sell = pd.read_excel('Акции.xlsx')
 
-
-# In[4]:
-
-
-sales.head()
-
-
+#%% [markdown]
 # Создание DataFrame для анализа зависимостей между переменными по артикулам
 
-# In[5]:
-
-
+#%%
 full_train_data = pd.DataFrame()
 
 for x in tqdm.tqdm(sales.index[:]):
@@ -75,11 +59,30 @@ for x in tqdm.tqdm(sales.index[:]):
 full_train_data.loc[:,'month'] = full_train_data.index.month
 
 
+#%%
+del sales
+del ap
+del stock
+del price
+del hierarchy
+del sell
+
+#%% [markdown]
+# Сохраняем собранные данные в файл для ускорения в будущем
+
+#%%
+full_train_data.to_excel('full_train_data.xlsx')
+
+#%% [markdown]
+# Выгружаем данные для обучения из файла
+
+#%%
+full_train_data = pd.read_excel('full_train_data.xlsx')
+
+#%% [markdown]
 # Создаем DF корелляции
 
-# In[16]:
-
-
+#%%
 df_columns_filter = ['sales','sales_1','sales_2','sales_12',
                      'sales_diff_1','sales_diff_2','sales_diff_12',
                      'stock_1','stock_2',
@@ -89,17 +92,13 @@ df_columns_filter = ['sales','sales_1','sales_2','sales_12',
                      'month','material']
 
 
-# In[17]:
-
-
+#%%
 corr_df = full_train_data[df_columns_filter].groupby('material').corr().abs().sales.fillna(0).unstack(level=0).T
 
-
+#%% [markdown]
 # Выделение групп артикулов для ращного прогнозирования на основе коррелляции
 
-# In[18]:
-
-
+#%%
 #Cluster the data
 kmeans = KMeans(n_clusters=5, random_state=0).fit(corr_df)
 labels = kmeans.labels_
@@ -112,26 +111,10 @@ corr_overal_df = corr_df.groupby('clusters').mean()
 
 corr_overal_norm_df = (corr_overal_df-corr_overal_df.min())/(corr_overal_df.max()-corr_overal_df.min())
 
-
-# Отображение кластеров и кластеров
-
-# In[19]:
-
-
-corr_overal_df.style.background_gradient(cmap='viridis')
-
-
-# In[20]:
-
-
-corr_overal_norm_df.style.background_gradient(cmap='viridis')
-
-
+#%% [markdown]
 # Вычисление колонок у кластера для использования для прогнозирования
 
-# In[21]:
-
-
+#%%
 # Будем отсекать по средней корелляции, за исключением артикулов, у которых нет данных
 threshold = corr_overal_df[corr_overal_df.sales > 0.1].mean()
 # Инициализация копии DF
@@ -143,26 +126,20 @@ for col in corr_overal_df.columns:
 corr_thresh_df.style.background_gradient(cmap='viridis')
 
 
-# In[22]:
-
-
+#%%
 def get_cluster_id(material_id):
     return corr_df.loc[material_id, 'clusters']
 
-
+#%% [markdown]
 # Создаем DF для всех материалов для обучения нейронки
 
-# In[24]:
-
-
+#%%
 train_data = full_train_data[df_columns_filter].merge(corr_df.clusters.to_frame(), how='left', left_on='material', right_index=True)
 
-
+#%% [markdown]
 # Создаем тренировочные данные
 
-# In[62]:
-
-
+#%%
 X = train_data[[col for col in train_data.columns if col not in ['sales', 'material']]]
 
 Y = train_data.sales.to_frame()
@@ -178,68 +155,43 @@ yscale=scaler.transform(Y)
 X_train, X_test, y_train, y_test = train_test_split(xscale, yscale)
 
 
-# In[144]:
-
-
+#%%
+# 82.6%
 model = Sequential()
-model.add(Dense(16, input_dim=16, kernel_initializer='normal'))
+model.add(Dense(32, input_dim=16, kernel_initializer='normal'))
 model.add(Dense(16))
 model.add(Dense(1))
 model.summary()
 
 
-# In[145]:
-
-
+#%%
 model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
 
 
-# In[ ]:
+#%%
+history = model.fit(X_train, y_train, epochs=200, batch_size=50, validation_data=(X_test, y_test), shuffle=True, verbose=1)
 
 
-history = model.fit(X_train, y_train, epochs=100, batch_size=50,  verbose=1, validation_split=0.2)
-
-
-# In[147]:
-
-
-print(history.history.keys())
-# "Loss"
+#%%
+plt.figure()
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
-plt.legend(['train', 'validation'], loc='upper left')
+plt.legend(['train', 'test'], loc='best')
 plt.show()
 
 
-# In[148]:
+#%%
+model.evaluate(X_test, y_test)
 
 
-model.evaluate(X_test, y_test, batch_size=50)
-
-
-# In[149]:
-
-
+#%%
 prediction = model.predict(X_test)
 
+accuracy = 1 - sum(abs(scaler.inverse_transform(prediction) - scaler.inverse_transform(y_test))) / sum(scaler.inverse_transform(y_test))
 
-# In[150]:
-
-
-scaler.inverse_transform(prediction[:10])
-
-
-# In[151]:
-
-
-scaler.inverse_transform(y_test[:10])
-
-
-# In[ ]:
-
-
+print('Total accuracy = %.2f%%' % (accuracy * 100))
 
 
